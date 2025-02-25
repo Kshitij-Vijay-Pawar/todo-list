154e8a0711c4f28a46ba9f188f9ced07334d2235
Kshitij <kshitij.vijay.pawar@gmail.com>
2025-02-25 21:38:48 +0530

    Implement task addition and dynamic task list update in ToDo List

--- scripts.js
+++ scripts.js
@@ -12,7 +12,7 @@ function updateTime() {
 
 updateTime(); 
 setInterval(updateTime, 1000); 
-
+// -------------------------------------------------------------------------------------
 
 function openModal() {
     document.getElementById("inputs").style.display = "flex";
@@ -20,4 +20,60 @@ function openModal() {
 
 function closeModal() {
     document.getElementById("inputs").style.display = "none";
+}
+// ----------------------------------------------------------------------------------------
+
+let tasks = [];
+
+document.getElementById("newTask").addEventListener("click", function(event) {
+
+    event.preventDefault();
+    addTask();
+});
+
+const addTask = () => {
+    const taskInput = document.getElementById("taskInput").value.trim();
+
+    if (taskInput) {
+        tasks.push({ text: taskInput, completed: false});
+        document.getElementById("taskInput").value = "";
+        updateTasksList();
+    }
+
+    console.log(tasks);
+
+}
+
+const updateTasksList = () => {
+    const tasksList = document.getElementById("tasksList");
+    tasksList.innerHTML = "";
+
+    tasks.forEach((task, index) => {
+        const taskElement = document.createElement("li");
+
+        taskElement.innerHTML = `
+            <li>
+                <div class="checkbox-wrapper-15">
+                    <input class="inp-cbx" id="cbx-1" type="checkbox" style="display: none;" />
+                    <label class="cbx" for="cbx-1">
+                        <span>
+                            <svg width="12px" height="9px" viewbox="0 0 12 9">
+                                <polyline points="1 5 4 8 11 1"></polyline>
+                            </svg>
+                        </span>
+                        <span>To-do</span>
+                    </label>
+                </div>
+                <button>Remove</button>
+            </li>
+        `;
+
+        taskElement.className = task.completed ? "completed" : "";
+        taskElement.addEventListener("click", () => {
+            tasks[index].completed = !tasks[index].completed;
+            updateTasksList();
+        });
+
+        tasksList.appendChild(taskElement);
+    });
 }
